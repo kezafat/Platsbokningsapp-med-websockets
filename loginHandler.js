c423ccf3b5665ef1ccb396ba298d6994ccc77aa5
Hamsho <hamidjade@gmail.com>
2019-01-26 22:30:51 +0100

    Added register-user route and save functionality

--- loginHandler.js
+++ loginHandler.js
@@ -11,10 +11,31 @@ module.exports = class LoginHandler {
     this.createRegisterRoute();
   }
 
-  createRegisterRoute() {
+  //MANUALLY add these routes (do not allow hackable queries)
+  async createRegisterRoute() {
     this.app.post('/register', async (req, res) => {
       let data = req.body;
-      // Now post some data about the user and store it in DB
+      let user = await this.User.findOne({ email: data.email });
+
+      if (user) {
+        // Mongoose unique returns result and reveals hashed pwd
+        // Already in DB, redirect user to login screen
+        res.json({"msg": "Plz log in, redirecting..."});
+        return;
+      }
+
+      if (!data.password) {
+        res.json({ "msg": "Pwd cannot be empty" });
+        return;
+      }
+
+      // Add the account
+      let encryptedPwd = await bcrypt.hash(data.password + passwordSalt, 10);
+      data.password = encryptedPwd;
+      let newUser = new this.User(data);
+      let err, result = await newUser.save().catch(error => err = error);
+      result.password = "hiddenPassword, no need to send this data back now is there ;D";
+      res.json(err || "ok");
     })
   }
 

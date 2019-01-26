01f23610fb03991dba314890662a946ab026157b
Hamsho <hamidjade@gmail.com>
2019-01-26 23:04:59 +0100

    Added a simple front-end form on pagecontent template
    So it is easier for ze others to try stuff out.

--- www/js/components/PageContent.js
+++ www/js/components/PageContent.js
@@ -1,3 +1,40 @@
 class PageContent extends Component {
+  constructor() {
+    super();
+    this.addEvents({
+      'click .addUser': 'addNewUser'
+    });
+    this.greeting = false;
+  }
+
+  async addNewUser() {
+    let userData = {
+      "email": $('#userEmail').val(),
+      "name": $('#userName').val(),
+      "password": $('#userPassword').val()
+    }
+
+    let that = this;
+    let saved = await $.ajax({
+      url: '/register',
+      method: 'POST',
+      contentType: 'application/json',
+      processData: false,
+      data: JSON.stringify(userData),
+      success: function (res) {
+        if(res == "ok"){
+          that.greeting = true;
+          that.greetUser();
+        } else{
+          console.log(res);
+        }
+      }
+    });
+  }
+
+  greetUser(){
+    this.greeting = true;
+    this.render();
+  }
 
 }

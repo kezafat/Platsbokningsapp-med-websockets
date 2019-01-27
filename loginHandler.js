936a6cbf2bf5d90cd8ef5f256ad22d9434df9025
Hamsho <hamidjade@gmail.com>
2019-01-27 14:50:27 +0100

    Loginhandler create user done
    Also added session data to user after account is successfully done.

--- loginHandler.js
+++ loginHandler.js
@@ -14,28 +14,42 @@ module.exports = class LoginHandler {
   //MANUALLY add these routes (do not allow hackable queries)
   async createRegisterRoute() {
     this.app.post('/register', async (req, res) => {
-      let data = req.body;
+      let err, data = req.body;
       let user = await this.User.findOne({ email: data.email });
 
       if (user) {
-        // Mongoose unique returns result and reveals hashed pwd
         // Already in DB, redirect user to login screen
-        res.json({"msg": "Plz log in, redirecting..."});
+        res.send("login");
         return;
       }
 
       if (!data.password) {
-        res.json({ "msg": "Pwd cannot be empty" });
+        res.send("error");
         return;
       }
 
-      // Add the account
+      // Add the account (I choose to handle all cases myself and respond with pure string responses that frontend can use to handle actions)
+      // We've been told that it is not good to expose that mongodb is being used, so this is my workaround.
       let encryptedPwd = await bcrypt.hash(data.password + passwordSalt, 10);
       data.password = encryptedPwd;
       let newUser = new this.User(data);
-      let err, result = await newUser.save().catch(error => err = error);
-      result.password = "hiddenPassword, no need to send this data back now is there ;D";
-      res.json(err || "ok");
+      let result = await newUser.save().catch(error => err = error);
+      // Actually not doing anything with err at all, if things do NOT go as planned, then they are errored and no need to tell frontend that
+      // Keeping err around for backend torubleshooting
+
+      // We check for clues in the result String as to how everything went
+      let resString = result + "";
+      resString = resString.toLowerCase();
+
+      if (resString.search('error') !== -1) {
+        res.send("error");
+      } else {
+        res.send("ok");
+        // Since all went well, also store some data in users session.
+        req.session.user = data.email;
+        req.session.auth = true;
+        req.session.save();
+      }
     })
   }
 

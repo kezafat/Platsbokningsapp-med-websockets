db21644dcfeabee211e37ee59f73bfa468202fa9
SimonPalmgren <simon.palmgren91@gmail.com>
2019-01-28 09:29:12 +0100

    added navigation

--- www/js/components/NavBar.js
+++ www/js/components/NavBar.js
@@ -1,3 +1,10 @@
 class NavBar extends Component {
-
+  constructor() {
+    super();
+    this.navItems = [
+      new NavItem('Start', '/'),
+      new NavItem('Filmer', '/filmer'),
+      new NavItem('Visningar', '/visningar'),
+    ];
+  }
 }

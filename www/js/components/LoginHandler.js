b3d1dfe036357df2e7fc235067596a3f5ab9797f
Hamsho <hamidjade@gmail.com>
2019-01-29 18:57:05 +0100

    Added redirect to frontend login (for existing users)

--- www/js/components/LoginHandler.js
+++ www/js/components/LoginHandler.js
@@ -2,15 +2,16 @@ class LoginHandler extends Component {
   constructor(navBar) {
     super();
     this.addEvents({
-      'click .addUser': 'addNewUser',
+      'click .addUser': 'registerUser',
       'click .loginUser': 'loginUser',
       'click .logOut': 'logOut'
     });
     this.navBar = navBar;
     this.auth = false;
-    this.register = false;
     this.loggedOut = false;
     this.user = "";
+    this.name = "";
+    this.notify = "";
 
     // Auto-login user if session auth is true
     this.checkLogin();
@@ -19,20 +20,19 @@ class LoginHandler extends Component {
   checkLogin() {
     let that = this;
     $.post('/login', function (res) {
-      console.log(res)
       if (res.msg === 'ok') {
         that.auth = true;
         that.user = res.user;
+        that.name = res.name;
         that.navBar.updateNavStatus("Mitt konto", that);
       } else {
-        that.register = true;
         that.auth = false;
         that.navBar.updateNavStatus("Logga in / Skapa konto", that);
       }
     });
   }
 
-  addNewUser() {
+  registerUser() {
     let userData = {
       "email": $('.regUserEmail').val(),
       "name": $('.regUserName').val(),
@@ -47,15 +47,17 @@ class LoginHandler extends Component {
       processData: false,
       data: JSON.stringify(userData),
       success: function (res) {
-        if (res.msg == "ok") {
+        if (res.msg === "ok") {
           that.auth = true;
           that.user = res.user;
+          that.name = res.name;
           $('#userModal').modal('hide').on('hidden.bs.modal', function (e) {
             that.navBar.updateNavStatus("Mitt konto", that);
           })
         } else if (res.msg === "login") {
           // Check for "ok". Anything else is error!
           console.warn("User already exists, go to login page plz");
+          that.redirectToLogin();
         }
       }
     });
@@ -76,8 +78,9 @@ class LoginHandler extends Component {
       processData: false,
       data: JSON.stringify(userData),
       success: function (res) {
-        if (res.msg == "ok") {
+        if (res.msg === "ok") {
           that.user = res.user;
+          that.name = res.name;
           that.auth = true;
           $('#userModal').modal('hide').on('hidden.bs.modal', function (e) {
             that.navBar.updateNavStatus("Mitt konto", that);
@@ -95,6 +98,7 @@ class LoginHandler extends Component {
       if (res.msg === 'ok') {
         that.loggedOut = true;
         that.auth = false;
+        that.name = "";
         that.user = "";
         $('#userModal').modal('hide').on('hidden.bs.modal', function (e) {
           that.navBar.updateNavStatus("Logga in / Skapa konto", that);
@@ -103,6 +107,16 @@ class LoginHandler extends Component {
     });
   }
 
+  redirectToLogin() {
+    let that = this;
+    $('#userModal').modal('hide');
+    $('#collapseTwo').collapse('toggle').on('hidden.bs.collapse', function () {
+      that.notify = "Användare finns redan, det är bara att logga in serru..";
+      that.render();
+      $('#userModal').modal('show');
+      $('#collapseOne').collapse('toggle');
+    })
+  }
 
 
 }

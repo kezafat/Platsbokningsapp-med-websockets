c0d3d84401b2b56fbe0c4b4ea03a0635e07eecbf
SimonPalmgren <simon.palmgren91@gmail.com>
2019-01-31 11:13:21 +0100

    added routs

---
+++ www/js/components/BookingConfirmation.js
@@ -0,0 +1,12 @@
+class BookingConfirmation extends Component {
+  constructor() {
+    super();
+    this.addRoute('/booking-bekräftelse', 'Boknings Bekräftelse');
+    this.name = "Boknings Bekräftelse"
+    // this.showInfo = [];
+    // this.getBookings();
+  }
+  // async getBookings() {
+  //   this.showInfo = await User.find(`.find({})`)
+  // }
+}

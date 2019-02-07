3c602797b908cff8f3d2c7d97b3389d29f9de05e
SimonPalmgren <simon.palmgren91@gmail.com>
2019-02-07 13:18:14 +0100

    methods for getting price and removed html from js

--- www/js/components/BookingConfirmation.js
+++ www/js/components/BookingConfirmation.js
@@ -4,53 +4,29 @@ class BookingConfirmation extends Component {
     this.addRoute('/booking-confirmation', 'Boknings Bekräftelse');
     this.user = new User(this.user)
     this.booking = {};
-    this.ticketPrice = {
-      adult: 85,
-      senior: 75,
-      kids: 65
-    }
     this.getSelectedBooking();
   }
 
-  async getSelectedBooking(){
+  async getSelectedBooking() {
     let id = location.search.split("id=");
     id.shift();
     id = id.join();
-    this.booking = await Booking.find(`.find({_id: "${id}"})`);
+    this.booking = await Booking.find(id);
     this.render();
   }
-  mount(){
+  mount() {
     this.getSelectedBooking();
   }
-  bookingTest(){
-    let booking = this.booking[0];
-    let title = booking.show.movie.title;
-    let img = booking.show.movie.images[0];
-    let auditoria = booking.show.auditorium.name;
-    let date = booking.show.date;
-    let time = booking.show.time;
-    let seats = booking.seats.join(", ");
-    let tempPrice = []
-    for (const price in booking.tickets ) {
-      for(let i = 0; i < price; i++){
-        tempPrice.push(this.ticketPrice[i][price]);
-      }
-    }
-    console.log(booking.tickets);
-    
-    function getSum(total, num){
-      return total + num
-    }
-    let totPrice = tempPrice.reduce(getSum);
-    return `<div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">${title}</h5>
-      <img src="/images/${img}">
-      <h6 class="card-subtitle mb-2 text-muted">${auditoria}</h6>
-      <p class="card-text">${time} ${date}</p>
-      <p class="card-text">Platser: ${seats}</p>
-      <p class="card-text font-weight-bold">Total Pris: ${totPrice}kr</p>
-    </div>
-  </div>`
+  get ticketPrice() {
+    return (this.booking.tickets.adult * 85) + (this.booking.tickets.senior * 75) + (this.booking.tickets.kids * 65);
+  }
+  get adultPrice(){
+    return (this.booking.tickets.adult * 85)
+  }
+  get seniorPrice(){
+    return (this.booking.tickets.senior * 75)
+  }
+  get kidsPrice(){
+    return (this.booking.tickets.kids * 65)
   }
 }

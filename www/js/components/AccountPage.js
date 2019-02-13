c7ef73af5038d8e504dd4d2641a0b28ca6a04209
Hamsho <hamidjade@gmail.com>
2019-02-13 12:47:55 +0100

    Finishing off

--- www/js/components/AccountPage.js
+++ www/js/components/AccountPage.js
@@ -28,8 +28,6 @@ class AccountPage extends Component {
     }
   }
 
-
-
   async fetchBookings() {
     this.userBookings = [];
     let jewser = await User.find(`.find({_id: "${this.userData._id}"})`);
@@ -47,91 +45,33 @@ class AccountPage extends Component {
   showUserBookings() {
     if (!AccountPage.auth) { return; }
     let html = "";
-    let oldBookings = [];
-    let actualBookings = [];
+    let previousBookings = [];
+    let currentBookings = [];
     let futureBookings = [];
-    let allBookings = [oldBookings, actualBookings, futureBookings];
-
-    function getTicketHtml(data) {
-      let adult = data.tickets.adult;
-      let senior = data.tickets.senior;
-      let kids = data.tickets.kids;
-      let seats = data.seats.join(', ');
-      let ticketID = data.ticketID || "N/A";
-      return `
-      <ul class="list-group">
-        <p class="font-weight-bold">Bokade biljetter</p>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-          Vuxna<span class="badge badge-primary badge-pill">${adult}</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-          Barn<span class="badge badge-primary badge-pill">${kids}</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-          Pensionärer<span class="badge badge-primary badge-pill">${senior}</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-          Platser: <span class="font-weight-italic">${seats}</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-          Bokningsnummer: <span class="font-weight-bold">${ticketID}</span>
-        </li>
-      </ul>
-      `
-    }
 
     for (let booking of this.userBookings) {
-      let tickets = getTicketHtml(booking);
-      let place = booking.show.auditorium.name;
       let todaysDate = new Date().toISOString().split('T')[0];
       let date = booking.show.date;
-      let time = booking.show.time;
-      let movieTitle = booking.show.movie.title;
-      let picpath = `/images/${booking.show.movie.images[0]}`;
-
       if (date == todaysDate) {
-        // TODAYS BOOKING
-        actualBookings.push(booking);
+        // TODAYS BOOKINGS
+        currentBookings.push(booking);
       } else if (date < todaysDate) {
         // OLD BOOKINGS
-        oldBookings.push(booking);
+        previousBookings.push(booking);
       } else {
         // FUTURE BOOKINGS
         futureBookings.push(booking);
       }
 
-      html += `
-      <div class="card text-center mb-2">
-        <div class="card-header">
-          <span class="font-weight-bold">${date}</span> klockan ${time} i
-          <span class="font-weight-light">${place}</span>
-        </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col">
-              <div class="text-center mb-2">
-                <h4>${movieTitle}</h4>
-                <img src="${picpath}" class="rounded" alt="Bild på omslaget för ${movieTitle}">
-              </div>
-            </div>
-            <div class="col">
-              ${tickets}
-            </div>
-          </div>
-        </div>
-        <!--
-        <div class="card-footer text-muted">
-          <a href="#" class="btn btn-danger">Avboka biljetterna?</a>
-        </div>
-        -->
-      </div>
-      `
+      // html += this.getBookingCard(booking);
     }
+    this.currentBookingsHTML = this.getBookingCard(currentBookings);
+    this.futureBookingsHTML = this.getBookingCard(futureBookings);
+    this.previousBookingsHTML = this.getBookingCard(previousBookings);
     // console.log(allBookings);
-    this.userBookingHTML = html;
+    // this.userBookingHTML = html;
     this.render();
   }
-
 
   async checkUserLoginState() {
     let state = await this.loginHandler.checkLogin()
@@ -199,7 +139,6 @@ class AccountPage extends Component {
 
   }
 
-
   updateUserData(state) {
     // Accept userdata from backend
     Object.assign(this.userData, state);
@@ -234,6 +173,86 @@ class AccountPage extends Component {
   clearNotifications() {
     this.loginNotify = "";
     this.registerNotify = "";
+  }
+
+  // Random funcs that just hang around for the fun of it
+  getTicketList(data) {
+    let adult = data.tickets.adult;
+    let senior = data.tickets.senior;
+    let kids = data.tickets.kids;
+    let seats = data.seats.join(', ');
+    let ticketID = data.ticketID || "N/A";
+
+    function badgeColor(count) {
+      return count > 0 ? 'badge-success' : 'badge-dark';
+    }
+
+    return `
+    <ul class="list-group">
+      <p class="font-weight-bold">Bokade biljetter</p>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        Vuxna<span class="badge ${badgeColor(adult)} badge-pill">${adult}</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        Barn<span class="badge ${badgeColor(kids)} badge-pill">${kids}</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        Pensionärer<span class="badge ${badgeColor(senior)} badge-pill">${senior}</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        Platser: <span class="font-weight-italic">${seats}</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        Bokningsnummer: <span class="font-weight-bold">${ticketID}</span>
+      </li>
+    </ul>
+    `
+  }
+
+  getBookingCard(bookings) {
+    console.log(bookings);
+    let tmp = "";
+    if (bookings.length < 1) {
+      tmp = `
+      <div class="alert alert-warning" role="alert">
+        Ingenting att visa här inte :(
+      </div>
+      `
+      return tmp;
+    }
+    for (const booking of bookings) {
+      let tickets = this.getTicketList(booking);
+      let place = booking.show.auditorium.name;
+      let date = booking.show.date;
+      let time = booking.show.time;
+      let movieTitle = booking.show.movie.title;
+      let picpath = `/images/${booking.show.movie.images[0]}`;
+      tmp += `
+
+      <div class="card text-center">
+        <div class="card-header">
+          <span class="font-weight-bold">${date}</span> klockan ${time} i
+          <span class="font-weight-light">${place}</span>
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col">
+              <div class="text-center mb-2">
+                <h4>${movieTitle}</h4>
+                <img src="${picpath}" class="rounded" alt="Bild på omslaget för ${movieTitle}">
+              </div>
+            </div>
+            <div class="col">
+              ${tickets}
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      `
+    }
+    return tmp;
   }
 
 }

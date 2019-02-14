513e9b29b25e2d45cbe4f55b4e673d828f86f0f8
SimonPalmgren <simon.palmgren91@gmail.com>
2019-02-14 11:06:58 +0100

    added new files to pagecontent

--- www/js/components/PageContent.js
+++ www/js/components/PageContent.js
@@ -6,7 +6,7 @@ class PageContent extends Component {
     this.moviePage = new MoviePage();
     this.aboutPage = new AboutUs();
     this.moviesSchedulePage = new MoviesSchedulePage();
-    this.bookingConfirmation = new BookingConfirmation();
+    this.bookingConfirmationContainer = new BookingConfirmationContainer();
     this.accountPage = new AccountPage(this.navBar);
     this.bookShowContainer = new BookShowContainer();
   }

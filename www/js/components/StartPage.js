ff19aeef763fd92c3f26f086e61373dc78b15b7d
Zoe Ali <zoeyzali@gmail.com>
2019-02-15 12:43:12 +0100

    halv way thru startpage

--- www/js/components/StartPage.js
+++ www/js/components/StartPage.js
@@ -1,9 +1,68 @@
+
 class StartPage extends Component {
 
-  constructor() {
+  constructor(show, movieSchedulePage) {
     super();
     this.addRoute('/', 'Startsida');
+    this.movies = [];
+    this.show = show;
+    this.movieSchedulePage = movieSchedulePage;
+    this.moviesHtml = 'loading';
+    this.getMovies();
+    this.getShows();
+    this.showsHtml = '';
+  }
+  async getMovies() {
+    this.movies = await Movie.find();
+    //console.log(this.movies)
+    this.createMoviesHtml()
+  }
+  createMoviesHtml() {
+    let html = '';
+    for (let movie of this.movies) {
+      html += `
+      <div class="card">
+       <img class= "card-img-top img-thumbnail" src="images/${movie.images[0]}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5><br>
+        <p class="card-text">${movie.genre} ${movie.productionYear}<br>${movie.director}</p>
+        <button href="/mitt-konto" class="btn btn-warning startbtn">Konto</button>
+        <button href="/movie-schedule-page" class="btn btn-warning startbtn">Visningar</button>
+      </div>
+      </div>
+        `
+    }
+    this.moviesHtml = html;
+    this.render();
+    //return 'html';
   }
 
+  async getShows() {
+    let id = this.id
+    let movie = this.movie;
+    let auditorium = this.auditorium;
+    this.shows = await Show.find(id, movie, auditorium);
+    //console.log(this.shows);
+    this.showCurrentShows();
+  }
 
+  showCurrentShows() {
+    let html = '';
+    // let currentDate = new Date().toISOString().split('T')[0];
+    // let showDate = show[0].currentDate
+    for (let show of this.shows) {
+      console.log(show.movie)
+      html += `
+      <ul>Datum
+       <li> ${show.id}</li>
+      </ul>
+      `
+    }
+    this.showsHtml = html;
+    this.render();
+  }
 }
+
+$('.carousel').carousel({
+  interval: 1000
+})

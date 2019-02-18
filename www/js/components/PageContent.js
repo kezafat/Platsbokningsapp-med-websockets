794ed65f042b8c874445403019deb94cf029c1f6
Hamsho <hamidjade@gmail.com>
2019-02-18 11:21:08 +0100

    Added scroll back to top functionality

--- www/js/components/PageContent.js
+++ www/js/components/PageContent.js
@@ -1,6 +1,9 @@
 class PageContent extends Component {
   constructor(navBar) {
     super();
+    this.addEvents({
+      'click .ttb': 'scrollUp'
+    })
     this.navBar = navBar;
     this.startPage = new StartPage();
     this.moviePage = new MoviePage();
@@ -9,5 +12,16 @@ class PageContent extends Component {
     this.bookingConfirmationContainer = new BookingConfirmationContainer();
     this.accountPage = new AccountPage(this.navBar);
     this.bookShowContainer = new BookShowContainer();
+  }
+
+  scrollUp() {
+    const scrollToTop = () => {
+      const c = document.documentElement.scrollTop || document.body.scrollTop;
+      if (c > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, c - c / 8);
+      }
+    };
+    scrollToTop();
   }
 }

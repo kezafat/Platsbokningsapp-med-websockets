63ea31d7a2b0b2a818f8e314922050f6f614bee0
SimonPalmgren <simon.palmgren91@gmail.com>
2019-02-19 13:49:06 +0100

    removed console.log

--- www/js/components/StartPage.js
+++ www/js/components/StartPage.js
@@ -16,13 +16,11 @@ class StartPage extends Component {
 
   async getMovies() {
     this.movies = await Movie.find();
-    //console.log(this.movies)
     this.createMoviesHtml()
   }
   createMoviesHtml() {
     let html = '';
     for (let movie of this.movies) {
-      console.log(movie.genre);
       html += `
       <div class="card">
        <img class= "card-img-top img-thumbnail" src="images/${movie.images[0]}" alt="movie-posters">
@@ -46,15 +44,12 @@ class StartPage extends Component {
     let movie = this.movie;
     let auditorium = this.auditorium;
     this.shows = await Show.find(id, movie, auditorium);
-    //console.log(this.shows); 
-    console.log(this.shows)
     this.showUpcomingShowsHTML();
   }
 
   showUpcomingShowsHTML() {
     let html = '';
     for (let show of this.shows) {
-      console.log(show.movie);
       html += `<ul> Datum
       <li> ${show.date} kl.${show.time}<br>${show.movie.title} ${show.auditorium.name}</li >
      </ul>`

23b65dd22f5d1b9e536947452563edc0718bde1e
SimonPalmgren <simon.palmgren91@gmail.com>
2019-02-19 14:10:13 +0100

    format

--- www/js/components/Movie.js
+++ www/js/components/Movie.js
@@ -3,14 +3,14 @@ class Movie extends Component {
   constructor(props) {
     super(props);
   }
-  setStars(){
-  let starsCount = this.reviews[0].stars;
-  let htmlStars = this.baseEl.find('.fa.fa-star');
-  for(let i = 0; i < starsCount; i++){
-    $(htmlStars[i]).addClass('checked');
+  setStars() {
+    let starsCount = this.reviews[0].stars;
+    let htmlStars = this.baseEl.find('.fa.fa-star');
+    for (let i = 0; i < starsCount; i++) {
+      $(htmlStars[i]).addClass('checked');
     }
   }
-  mount(){
+  mount() {
     this.setStars();
   }
 }

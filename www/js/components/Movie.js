00d11f2ab735052ba2cc6411b8834360029e06a8
SimonPalmgren <simon.palmgren91@gmail.com>
2019-02-19 13:50:04 +0100

    added rating-stars to movie-page and new buttons

--- www/js/components/Movie.js
+++ www/js/components/Movie.js
@@ -2,17 +2,15 @@ class Movie extends Component {
 
   constructor(props) {
     super(props);
-
   }
-
-  // $('#link').click(function() {
-  //   var src = 'https://www.youtube.com/embed/${this.youtubeTrailers[0];autoplay=1';
-  //   $('#myModal').modal('show');
-  //   $('#myModal iframe').attr('src', src);
-  // });
-
-  // $('#myModal button').click(function() {
-  //   $('#myModal iframe').removeAttr('src');
-  // });
-
+  setStars(){
+  let starsCount = this.reviews[0].stars;
+  let htmlStars = this.baseEl.find('.fa.fa-star');
+  for(let i = 0; i < starsCount; i++){
+    $(htmlStars[i]).addClass('checked');
+    }
+  }
+  mount(){
+    this.setStars();
+  }
 }

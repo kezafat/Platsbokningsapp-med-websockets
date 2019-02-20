039598f41ee39bd15ecfb6faaefef63812cf825a
melli <melsie78@hotmail.com>
2019-02-20 10:01:50 +0100

    movie-detail-page

--- www/js/components/MovieDetail.js
+++ www/js/components/MovieDetail.js
@@ -1,8 +1,39 @@
 class MovieDetail extends Component {
 
-  constructor(movie){
+  constructor(movie) {
     super();
     this.movie = movie;
+    this.movieTimes = "";
+    this.actorsList = "";
+    this.getAllDates();
+    this.getActorList();
   }
-  
+
+  getActorList(){
+    let actorsList = "";
+    for(let actor of this.movie.actors) {
+      this.actorsList += `<li>${actor}</li>`;
+      console.log(actor);
+    }
+    actorsList += '</ul>'
+    return actorsList;
+  }
+
+
+  getAllDates() {
+    let i = 0; 
+    for (let show of this.movie.shows) {
+      // console.log(show);
+      // console.log(this.movie);
+
+      this.movieTimes += `<p>${show.date}</p>`
+      this.movieTimes += `<p>kl ${show.time}</p>`
+      this.movieTimes += `<p>i ${show.auditorium.name} </p><hr>`
+      i++;
+      if(i == 5){ break; }
+     
+    }
+
+    this.render();
+  }
 }

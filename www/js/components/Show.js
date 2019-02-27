9acc5342ded5b59dccdf042fba1f179831e0d44b
SimonPalmgren <simon.palmgren91@gmail.com>
2019-02-27 11:37:02 +0100

    format js

--- www/js/components/Show.js
+++ www/js/components/Show.js
@@ -1,6 +1,6 @@
 class Show extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     // convert raw js object this.movie to a real instance of Movie
     this.movie = new Movie(this.movie);

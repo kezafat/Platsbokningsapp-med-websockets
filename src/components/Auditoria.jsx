85912129c948acd41a0c4a410460c81e19a9ef71
Elias Hjelm <elias.hj@gmail.com>
2019-03-01 16:06:53 +0100

    Load auditoria from json, shows from db, change npm start script

--- src/components/Auditoria.jsx
+++ src/components/Auditoria.jsx
@@ -3,7 +3,6 @@ import {
   Container,
   Row,
   Card,
-  Spinner,
   Col,
   CardImg,
   CardBody,
@@ -14,47 +13,32 @@ import { Link } from 'react-router-dom'
 class Auditoria extends Component {
   constructor(props) {
     super(props)
-    this.state = { fetched: false }
-    this.fetchAuditoria()
-  }
-
-  fetchAuditoria = async () => {
-    const response = await fetch('http://localhost:3000/json/auditoria', {
-      method: 'GET'
-    });
-    this.auditoria = await response.json();
-    this.setState({ fetched: true })
+    this.auditoria = require('../json/auditoria.json')
   }
 
   render() {
-    if (this.state.fetched) {
-      return (
-        <Container>
-          <Row>
-            {
-              this.auditoria.map(auditorium => (
-                <Col xs="12" md="4" className="d-flex my-3" key={auditorium.name}>
-                  <Card>
-                    <CardImg top src={require('../images/' + auditorium.imageSmall)} />
-                    <CardBody className="p-3">
-                      <h5 className="card-title">{auditorium.name}</h5>
-                      <CardText>{auditorium.descriptionShort}</CardText>
-                    </CardBody>
-                    <div className="p-3">
-                      <Link to={'/biografer/' + auditorium._id} className="btn btn-outline-danger">MER INFO</Link>
-                    </div>
-                  </Card>
-                </Col>
-              ))
-            }
-          </Row>
-        </Container>
-      )
-    } else {
-      return (
-        <Spinner color="primary" />
-      )
-    }
+    return (
+      <Container>
+        <Row>
+          {
+            this.auditoria.map(auditorium => (
+              <Col xs="12" md="4" className="d-flex my-3" key={auditorium.name}>
+                <Card>
+                  <CardImg top src={require('../images/' + auditorium.imageSmall)} />
+                  <CardBody className="p-3">
+                    <h5 className="card-title">{auditorium.name}</h5>
+                    <CardText>{auditorium.descriptionShort}</CardText>
+                  </CardBody>
+                  <div className="p-3">
+                    <Link to={'/biografer/' + auditorium._id} className="btn btn-outline-danger">MER INFO</Link>
+                  </div>
+                </Card>
+              </Col>
+            ))
+          }
+        </Row>
+      </Container>
+    )
   }
 }
 
--- src/components/Auditorium.jsx
+++ src/components/Auditorium.jsx
@@ -14,21 +14,19 @@ class Auditorium extends Component {
   constructor(props) {
     super(props)
     this.state = { fetched: false }
-    this.fetchAuditorium()
+    this.auditorium = require('../json/auditoria.json').find(auditorium => auditorium._id === this.props.match.params.id)
+    this.fetchShows()
   }
 
   get totalSeats() {
     return this.auditorium.seats.reduce((acc, cur) => acc + cur, 0)
   }
 
-  fetchAuditorium = async () => {
-    if (this.state.fetched) {
-      this.setState({ fetched: false })
-    }
-    const auditoriumId = this.props.match.params.id
-    const result = await fetch('http://localhost:3000/json/auditoria/' + auditoriumId)
-    this.auditorium = await result.json()
-    if (this.auditorium.shows) {
+  fetchShows = async () => {
+    const result = await fetch('http://localhost:3000/json/auditoria/' + this.props.match.params.id)
+    const auditorium = await result.json()
+    this.shows = auditorium.shows
+    if (this.shows) {
       this.sortAndFilterShows()
     }
     this.setState({ fetched: true })
@@ -39,7 +37,7 @@ class Auditorium extends Component {
     //set time format and add current date to first movie viewing
     const currentDate = now[0];
     const currentTime = now[1].split(':').slice(0, 2).join(':');
-    this.auditorium.shows = this.auditorium.shows.filter(show => {
+    this.shows = this.shows.filter(show => {
       if (show.date > currentDate) {
         return true
       } else if (show.date === currentDate && show.time > currentTime) {
@@ -48,7 +46,7 @@ class Auditorium extends Component {
         return false
       }
     });
-    this.auditorium.shows.sort((a, b) => {
+    this.shows.sort((a, b) => {
       if (a.date < b.date) { return -1 }
       else if (a.date > b.date) { return 1 }
       else if (a.time < b.time) { return -1 }
@@ -58,60 +56,62 @@ class Auditorium extends Component {
   }
 
   render() {
-    if (!this.state.fetched) {
-      return <Spinner color="secondary" />
-    } else {
-      return (
-        <Container>
-          <img src={require('../images/' + this.auditorium.imageBig)} alt="Auditorium" className="img-fluid" />
-          <Row noGutters>
-            <Col xs="12" md="6" className="d-flex">
-              <Card>
-                <CardBody>
-                  <h3 className="card-title">{this.auditorium.name}</h3>
-                  <p>{this.auditorium.descriptionLong}</p>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col xs="12" md="6" className="d-flex">
-              <Card className="flex-grow-1">
-                <CardBody>
-                  <h5 className="card-title">Info</h5>
-                  <p>Högtalarsystem: {this.auditorium.soundSystem}</p>
-                  <p>Projektor: {this.auditorium.projector}</p>
-                  <p>Godisbutik: {this.auditorium.candyStore}</p>
-                  <p>Serveringstillstånd: {this.auditorium.alcohol}</p>
-                  <p>Platser: {this.totalSeats}</p>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col xs="12">
-              <Card>
-                <CardBody>
-                  <h3 className="card-title">Kommande Visningar</h3>
-                    {
-                      this.auditorium.shows.slice(0, 3).map(show => {
-                        const freeSeats = this.totalSeats - show.bookings.map(booking => booking.seats).flat().length;
-                        return (
-                            <Row className="my-3 text-white">
-                              <Col xs="12" sm="auto">{show.date + ' ' + show.time}</Col>
-                              <Col xs="12" sm="auto">{show.movie.title}</Col>
-                              <Col xs="12" className="d-flex justify-content-end col-md">
-                                <Link to={'/visningar/' + show._id} className="btn btn-outline-danger btn-sm float-md-right">
-                                  {freeSeats} kvar av {this.totalSeats} ->
+    return (
+      <Container>
+        <img src={require('../images/' + this.auditorium.imageBig)} alt="Auditorium" className="img-fluid" />
+        <Row noGutters>
+          <Col xs="12" md="6" className="d-flex">
+            <Card>
+              <CardBody>
+                <h3 className="card-title">{this.auditorium.name}</h3>
+                <p>{this.auditorium.descriptionLong}</p>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" md="6" className="d-flex">
+            <Card className="flex-grow-1">
+              <CardBody>
+                <h5 className="card-title">Info</h5>
+                <p>Högtalarsystem: {this.auditorium.soundSystem}</p>
+                <p>Projektor: {this.auditorium.projector}</p>
+                <p>Godisbutik: {this.auditorium.candyStore}</p>
+                <p>Serveringstillstånd: {this.auditorium.alcohol}</p>
+                <p>Platser: {this.totalSeats}</p>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12">
+            <Card>
+              <CardBody>
+                <h3 className="card-title">Kommande Visningar</h3>
+                {
+                  (!this.state.fetched)
+                    ?
+                    <div className="d-flex justify-content-center">
+                      <Spinner color="secondary" />
+                    </div>
+                    :
+                    this.shows.slice(0, 3).map(show => {
+                      const freeSeats = this.totalSeats - show.bookings.map(booking => booking.seats).flat().length;
+                      return (
+                        <Row className="my-3 text-white" key={show._id}>
+                          <Col xs="12" sm="auto">{show.date + ' ' + show.time}</Col>
+                          <Col xs="12" sm="auto">{show.movie.title}</Col>
+                          <Col xs="12" className="d-flex justify-content-end col-md">
+                            <Link to={'/visningar/' + show._id} className="btn btn-outline-danger btn-sm float-md-right">
+                              {freeSeats} kvar av {this.totalSeats} ->
                                 </Link>
-                              </Col>
-                            </Row>
-                        )
-                      })
-                    }
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      )
-    }
+                          </Col>
+                        </Row>
+                      )
+                    })
+                }
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    )
   }
 }
 

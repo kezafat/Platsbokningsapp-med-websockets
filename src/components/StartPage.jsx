6a510135fdc81c16b9053d180590ceb56dcc2325
Zoe Ali <zoeyzali@gmail.com>
2019-03-04 09:48:25 +0100

    startpage react ref

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import {
+  Container, Row, Col, Card, Button, CardImg, CardTitle, CardText, CardGroup,
+  CardSubtitle, CardBody
+} from 'reactstrap';
 
 class StartPage extends Component {
+getfilmsData(){
+  this.movies = require('../movies.json');
+}
+
+
   render() {
     return (
-      <div>StartPage</div>
+      
     )
   }
 }

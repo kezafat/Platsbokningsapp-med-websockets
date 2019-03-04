195770b941488d4ce4bb15bb410579284c0adba8
Zoe Ali <zoeyzali@gmail.com>
2019-03-04 12:01:56 +0100

    moviescard startpage plus movies.json

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -5,14 +5,33 @@ import {
 } from 'reactstrap';
 
 class StartPage extends Component {
-getfilmsData(){
-  this.movies = require('../movies.json');
-}
-
-
+  constructor(props) {
+    super(props);
+    this.movies = require('../json/movies.json');
+  }
   render() {
-    return (
-      
+    return (<Container className="StartPage">
+      <Row>
+        <Col xs="12" md="12" className="d-flex">
+          {this.movies.map((movie) => {
+            return <div className="Movies">
+              <CardGroup>
+                <Card>
+                  <CardBody>
+                    <CardTitle><h4>{movie.title}</h4></CardTitle>
+                    <CardImg top width="100%" src={require('../images/' + movie.images)} alt="Posters" />
+                    <CardSubtitle><p>{movie.productionCountries} || {movie.director}<br />
+                      {movie.genre}, {movie.productionYear} </p></CardSubtitle>
+                    <CardText><p>{movie.description.substr(0, 100) + '...'}</p></CardText>
+                    <Button to={'/visningar/'} className="btn btn-outline-danger">Visningar</Button>
+                  </CardBody>
+                </Card>
+              </CardGroup>
+            </div>
+          })}
+        </Col></Row>
+    </Container>
+
     )
   }
 }

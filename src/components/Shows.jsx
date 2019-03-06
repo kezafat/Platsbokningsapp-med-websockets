f747d3fa8d54fdb9bcf0f7edfc3f71621b8dee24
melli <melsie78@hotmail.com>
2019-03-06 14:56:45 +0100

    movie schedule page in react

---
+++ src/components/Shows.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import { Row } from "reactstrap";
+import Day from "./Day";
+
+class Shows extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.days = [];
+    this.allShows = [];
+    this.fetchShows();
+  }
+
+  async fetchShows() {
+    let now = new Date().toISOString().split("T");
+    let currentDate = this.currentDate = now[0];
+    let currentTime = now[1]
+      .split(":")
+      .slice(0, 2)
+      .join(":");
+    this.allShows = await fetch("http://localhost:3000/json/shows/");
+    this.allShows = await this.allShows.json();
+    this.allShows = this.allShows.filter(show => {
+      if (show.date > currentDate) {
+        return true;
+      } else if (show.date === currentDate && show.time > currentTime) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    
+    this.allShows.sort((a, b) => {
+      if (a.date < b.date) {
+        return -1;
+      } else if (a.date > b.date) {
+        return 1;
+      } else if (a.time < b.time) {
+        return -1;
+      } else if (a.time > b.time) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    if (this.allShows.length === 0) {
+      return;
+    } else {
+      this.allShows.length = 21;
+    }
+
+    let firstDate = this.allShows[0].date;
+    // create a day with the same date as the first show
+    this.days = [{ date: firstDate, shows: [] }];
+    for (let show of this.allShows) {
+      let currentDay = this.days[this.days.length - 1];
+      // if the date of the show isn't the current day
+      // create a new day
+      if (show.date !== currentDay.date) {
+        currentDay = { date: show.date, shows: [] };
+        this.days.push(currentDay);
+      }
+      // add the show to the current day object
+      currentDay.shows.push(show);
+    }
+    console.log(this.days);
+    
+
+    // render
+      this.setState(state => this);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+      <Row />
+        <div className="movie-schedule-page">
+          <h2 className="mb-3 ml-3">Filmvisningar</h2>
+          
+          {this.days.map((day, index) => (
+            <Day key={index} data={day} />
+          ))}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Shows;

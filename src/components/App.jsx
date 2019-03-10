29fcc0dd4f6265845ed5c396c80591c0974d7279
Hamsho <hamidjade@gmail.com>
2019-03-10 15:37:19 +0100

    Added AccountLink to app.jsx
    global.user stores response from server that everyone can enjoy in their templates.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,56 @@
 import React, { Component } from 'react';
 import '../avoidFOUC';
-import NavBar from  './NavBar'
+import NavBar from './NavBar'
 import Footer from './Footer'
 import MoviesContainer from './MoviesContainer'
 import ShowContainer from './ShowContainer'
 import StartPage from './StartPage'
 import Auditoria from './Auditoria'
 import Auditorium from './Auditorium'
-import { Route } from 'react-router-dom'
+import AccountPage from './account/AccountPage'
+import LoginPage from './account/LoginPage'
+import RegisterPage from './account/RegisterPage'
+import UserPage from './account/UserPage'
+import { Route, } from 'react-router-dom'
+import { Spinner, } from 'reactstrap'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.setAuth = this.setAuth.bind(this);
+    global.auth = false;
+    global.user = {};
+    this.state = {
+      setAuth: this.setAuth,
+      display: "welcome",
+      linkVal: <Spinner />,
+    }
+  }
+
+  setAuth(data, userdata = {}) {
+    global.auth = data.authStatus;
+    global.user = userdata;
+    this.setState(data);
+  }
+
   render() {
     return (
       <div className="App">
-      <header>
-        <NavBar />
-      </header>
-      <main className="container">
-        <Route exact path="/" component={StartPage} />
-        <Route path="/filmer" component={MoviesContainer} />
-        <Route path="/visningar" component={ShowContainer} />
-        <Route exact path="/biografer" component={Auditoria} /> 
-        <Route path ="/biografer/:name" component={Auditorium} />
-      </main>
-      <Footer />
+        <header>
+          <NavBar state={this.state} />
+        </header>
+        <main className="container">
+          <Route exact path="/" component={StartPage} />
+          <Route path="/filmer" component={MoviesContainer} />
+          <Route path="/visningar" component={ShowContainer} />
+          <Route exact path="/biografer" component={Auditoria} />
+          <Route path="/biografer/:name" component={Auditorium} />
+          <Route exact path="/konto" render={() => <div><AccountPage state={this.state} /></div>} />
+          <Route exact path="/logga-in" render={() => <div><LoginPage state={this.state} /></div>} />
+          <Route exact path="/registrera-konto" render={() => <div><RegisterPage state={this.state} /></div>} />
+          <Route exact path="/mitt-konto" render={() => <div><UserPage state={this.state} /></div>} />
+        </main>
+        <Footer />
       </div>
     );
   }

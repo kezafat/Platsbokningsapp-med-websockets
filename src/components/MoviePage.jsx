bc6b7b7c18e68702b1fda4a64cec01331f252ea4
SimonPalmgren <simon.palmgren91@gmail.com>
2019-03-11 09:30:54 +0100

    added comments

--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -29,6 +29,7 @@ class MoviePage extends Component {
   }
 
   getNextShow(shows) {
+    {/*Getting the next coming show */}
     shows.sort(function (a, b) {
       return new Date(a.date + ' ' + a.time) - new Date(b.date + ' ' + b.time);
     });
@@ -45,11 +46,8 @@ class MoviePage extends Component {
     await this.fetchMovies()
   }
 
-  /**
-   * @blame should be part of the react element, not working on the DOM like now..
-   * 
-   */
   setStars() {
+    {/* Method for adding the star */}
     for (let i = 0; i < this.movies.length; i++) {
       let starsCount = this.movies[i].reviews[0].stars;
       let htmlStars = document.getElementsByClassName("fa-star");

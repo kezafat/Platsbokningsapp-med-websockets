39c867b389d52436c20bae0852e600a8eff6837e
melli <melsie78@hotmail.com>
2019-03-11 12:03:30 +0100

    booking confirm edits

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import NavBar from './NavBar'
 import Footer from './Footer'
 //import MoviesContainer from './MoviesContainer'
 import Shows from './Shows'
-// import BookingConfirmationContainer from './BookingConfirmationContainer'
+import BookingConfirmationContainer from './BookingConfirmationContainer'
 import MoviePage from './MoviePage'
 import MovieDetail from './MovieDetail'
 import StartPage from './StartPage'
@@ -22,19 +22,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <header>
-        <NavBar />
-      </header>
-      <main className="main-content container mt-4 mb-3">
-        <Route exact path="/" component={StartPage} />
-        <Route exact path="/visningar" component={Shows} />
+        <header>
+          <NavBar />
+        </header>
+        <main className="main-content container mt-4 mb-3">
+          <Route exact path="/" component={StartPage} />
+          {/* <Route exact path="/visningar" component={BookingConfirmationContainer} /> */}
           <Route path="/visningar/:auditorium/:date/:time" component={BookShow} />
-        <Route exact path="/filmer" component={MoviePage} />
-        <Route path="/filmer/:title" component={MovieDetail} />
-        <Route exact path="/biografer" component={Auditoria} /> 
-        <Route path ="/biografer/:name" component={Auditorium} />
-      </main>
-      <Footer />
+          <Route exact path="/filmer" component={MoviePage} />
+          <Route path="/filmer/:title" component={MovieDetail} />
+          <Route path="/visningar/" component={Shows} />
+          <Route path="/bokningsbekräftelse/" component={BookingConfirmationContainer} />
+          <Route exact path="/biografer" component={Auditoria} />
+          <Route path="/biografer/:name" component={Auditorium} />
+        </main>
+        <Footer />
       </div>
     );
   }
--- src/components/BookingConfirmationContainer.jsx
+++ src/components/BookingConfirmationContainer.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import BookingConfirmation from "./BookingConfirmation";
+import { Spinner } from 'reactstrap';
 
 class BookingConfirmationContainer extends Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class BookingConfirmationContainer extends Component {
     // if (id === null) {
     //   return
     // };
-    
+
     let booking = await fetch("http://localhost:3000/json/shows/");
     this.bookingConfirmation = new BookingConfirmation(booking);
     this.setState(state => this);
@@ -32,15 +33,7 @@ class BookingConfirmationContainer extends Component {
   render() {
     return (
       <div class="booking-confirmation-container">
-        $
-        {this.fetchedBooking
-          ? this.bookingConfirmation
-          : `<div
-    class="spin-container d-flex justify-content-center center-spinner">
-    <div class="spinner-border" role="status">
-      <span class="sr-only">Loading...</span>
-    </div>
-  </div>`}
+        {this.fetchedBooking ? this.bookingConfirmation : <Spinner />}
       </div>
     );
   }

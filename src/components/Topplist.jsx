1dfa12ab31695b0e59e63d0d4dc4eefdb3370faa
melli <melsie78@hotmail.com>
2019-03-15 10:55:50 +0100

    topplist establishment

---
+++ src/components/Topplist.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Topplist extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>hello!</div>
+     );
+  }
+}
+ 
+export default Topplist;

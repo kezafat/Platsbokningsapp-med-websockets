29dedfc033a5f6927cdbbfbd76d8c50e2101f526
Zoe Ali <zoeyzali@gmail.com>
2019-03-18 15:36:24 +0100

    startpage done
    Up for testing

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -10,24 +10,42 @@ class Calendar extends Component {
     fetch(`http://localhost:3000/json/shows/`)
       .then(res => res.json())
       .then(json => this.setState({ data: json }));
-
-
   }
+
   render() {
-    return (
-      <Row className="shows-calendar mx-2 rounded">
+    const now = new Date().toISOString().split('T');
+    const currentDate = now[0];
+    const currentTime = now[1];
+    return (<Row className="shows-calendar mx-2 rounded">
+      <div className="text-center ml-2 mb-2">
+        <h2>Kalendarium</h2>
+      </div>
+      <div>
         <div className="calender-wrapper">
-          <div>
-            <h2 className="text-center">Kalendarium</h2>
-          </div>
-          <div className="text-center ml-2 mb-2">
-            {this.state.data.slice(1, 12).sort().map((show, _id) => (
-              <p key={_id}>{show.date} &nbsp; <strong>kl.</strong> &nbsp;{show.time}<br />
+          {this.state.data.filter(show => {
+            if (show.date === currentDate) {
+              return true
+            } else if (show.date >= currentDate && show.time > currentTime) {
+              return true
+            } else {
+              return false
+            }
+          }).sort((
+            function (a, b) {
+              if (a.date < b.date) { return -1 }
+              else if (a.date > b.date) { return 1 }
+              else if (a.time < b.time) { return -1 }
+              else if (a.time > b.time) { return 1 }
+              else {
+                return 0;
+              }
+            })).slice(0, 10).map((show) => (
+              <p key={show._id}>{show.date} &nbsp; <strong>kl.</strong> &nbsp;{show.time}<br />
                 {show.movie.title}</p>
             ))}
-          </div>
         </div>
-      </Row>
+      </div>
+    </Row>
     )
   }
 }

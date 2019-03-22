efd83d53943fc2f2c8ff7b09f2a3bed0b5ed5c1e
SimonPalmgren <simon.palmgren91@gmail.com>
2019-03-22 12:12:05 +0100

    margin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ class Footer extends Component {
 
   render() {
     return (
-      <footer>
+      <footer className="mt-4">
         <section>
           <div className="footer-text">
             <h3>Adress</h3>
